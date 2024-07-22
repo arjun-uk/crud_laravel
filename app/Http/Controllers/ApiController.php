5199d737fe891355fcb4de1f976b7270f8e18192
arjunuk <arjunuk@nexbex.net>
2024-07-22 11:10:17 +0530

    Remove firebase.json from commit

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -13,6 +13,8 @@ use Kreait\Firebase\Exception\FirebaseException;
 
 class ApiController extends Controller
 {
+
+    
     public function login_api(Request $request)
     {
         try {
@@ -301,13 +303,25 @@ class ApiController extends Controller
             ];
 
             
-         
-            $serviceAccount = storage_path('firebase.json');
+            $firebaseCredentials = [
+                "type" => env('FIREBASE_TYPE'),
+                "project_id" => env('FIREBASE_PROJECT_ID'),
+                "private_key_id" => env('FIREBASE_PRIVATE_KEY_ID'),
+                "private_key" => str_replace("\\n", "\n", env('FIREBASE_PRIVATE_KEY')),
+                "client_email" => env('FIREBASE_CLIENT_EMAIL'),
+                "client_id" => env('FIREBASE_CLIENT_ID'),
+                "auth_uri" => env('FIREBASE_AUTH_URI'),
+                "token_uri" => env('FIREBASE_TOKEN_URI'),
+                "auth_provider_x509_cert_url" => env('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
+                "client_x509_cert_url" => env('FIREBASE_CLIENT_X509_CERT_URL'),
+                "universe_domain" => env('FIREBASE_UNIVERSE_DOMAIN')
+            ];
+            //$serviceAccount = json_decode(env('FCM_CREDENTIALS'), true);
            
 
             // Initialize Firebase
             $firebase = (new Factory)
-            ->withServiceAccount($serviceAccount);
+            ->withServiceAccount($firebaseCredentials);
 
         $messaging = $firebase->createMessaging();
 
